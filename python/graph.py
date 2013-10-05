cfc5c079a35343973158912e856267045d168bbe
r <brownrp1@brownrp1-laptop.jhuapl.edu>
2013-10-05 09:34:13 -0400

    Adding notations to graph

--- python/graph.py
+++ python/graph.py
@@ -3,7 +3,14 @@ import matplotlib.pyplot as plt
 
 month = [4, 5, 6, 7, 8, 9]
 expense = [758.19, 942.77, 863.38, 749.51, 674.18, 642.26]
-plt.plot(month, expense)
+plt.plot(month, expense, 'ro')
+for x, y in zip(month, expense):
+	plt.annotate(str(y), xy = (x, y), xytext = (-20, 20),
+	textcoords = 'offset points', ha = 'right', va = 'bottom',
+	bbox = dict(boxstyle = 'round,pad=0.5', fc = 'yellow', alpha = 0.5),
+	arrowprops = dict(arrowstyle = '->', connectionstyle = 'arc3,rad=0'))
+
+
 plt.xlabel('month')
 plt.ylabel('expense')
 plt.show()
