6b609a653a5b9ad077a4e9c6fcea50c9e622471b
r <brownrp1@brownrp1-laptop.jhuapl.edu>
2013-11-08 12:49:37 -0500

    Tons more examples for make ninja magic

--- make/extend.mk
+++ make/extend.mk
@@ -7,9 +7,41 @@ define myfunc
 # won't be a target representing a real file
 .PHONY: $(1)
 
-# In a user defined function, you have to double dollar
-# sign user variables to get them to function correctly
+# When using eval function, you may have to use double dollar
+# sign on user variables to get them to function correctly
+# eval wants to immediately resolve all variables when forming
+# the new syntax. variables $(1) and $(2) should work fine
+# since those arguments have already been passed.. but $@ and $^
+# are special variables dependent on the target..which won't be
+# resolved til the target is hit at the end
+#
+# So we escape them with an extra dollar sign, so that eval
+# maintains them as $@ and $^
+# ANOTHER_VAR needs the escape too, because it hasn't actually been
+# defined yet as eval is going through to resolve all variables.
+# ANOTHER_VAR's assignment is in the very body that eval must resolve first
+ANOTHER_VAR := "more text"
 $(1): $(2)
-	@echo "$$@ and $$^"
+	@echo "$$@ and $$^ and $$(ANOTHER_VAR)"
 
+endef
+
+# function to assist with directory target implementing mkdir -p
+define buildpath
+TEMP_DIR+=$(1)/
+space :=
+space +=
+TEMP_DIR := $$(subst $$(space),,$$(TEMP_DIR))
+PATHWAYS += $$(TEMP_DIR)
+endef
+
+# Without eval, this is actually going to return the string "MISLEADING += test"
+# instead of setting MISLEADING
+define bluntprint
+MISLEADING += $(1)
+# If there was another command here there would be a syntax error
+endef
+
+define parseprint
+$(subst e,E,$(1))
 endef
